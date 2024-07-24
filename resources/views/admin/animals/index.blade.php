d0e3b9460983f0d7d6f2f6bb6b75a1624e45a71e
Luca Sammarco <luca.sammarco7@gmail.com>
2024-07-24 17:35:17 +0200

    added delete

--- resources/views/admin/animals/index.blade.php
+++ resources/views/admin/animals/index.blade.php
@@ -17,6 +17,16 @@
                         <p class="card-text"><strong>Species:</strong> {{ $animal->species }}</p>
                         <p class="card-text"><strong>Age:</strong> {{ $animal->age }} years old</p>
                         <p class="card-text">{{ $animal->description }}</p>
+
+                        <a href="{{ route('admin.animals.show', $animal) }}" class="btn btn-primary">Show</a>
+
+                        <a href="{{ route('admin.animals.edit', $animal) }}" class="btn btn-secondary">Edit</a>
+
+                        <form action="{{ route('admin.animals.destroy', $animal) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
                     </div>
                 </div>
             </div>
